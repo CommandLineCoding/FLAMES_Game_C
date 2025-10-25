6a99a2cb6d18813ab7566686da6321331abaa9e3
Suman Biswas <89653112+mrsumanbiswas@users.noreply.github.com>
2025-10-25 12:46:42 +0000

    fun proto

---
+++ 01/main.c
@@ -0,0 +1,19 @@
+/*
+input: male.txt & female.txt
+process: random selection of names (one from male and one from female); FLAMES calculation
+output: Male Name; Female Name; FLAMES score
+*/
+
+#include<stdio.h>
+
+char flames(char[], char[]);
+char* strmal(char[]);
+int freq_char(char[]);
+int rem_char(char[]);
+char flames_char(int);
+
+
+int main(){
+
+    return 0;
+}
